ba0d0e1b2cf514ea7a55b0e7e1ed8e94ca33ab06
nishthasharma01 <54005937+nishthasharma01@users.noreply.github.com>
2019-08-11 16:09:11 -0700

    Add files via upload

---
+++ Test_2_This_Course_tab.java
@@ -0,0 +1,39 @@
+package Testing_Spec;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import Browser_info.Browser_class;
+import Elements_methods.App_elements;
+import library.Utility;
+
+public class Test_2_This_Course_tab {
+
+	@Test
+	public void verify_this_course_tab () throws Exception
+	{
+				
+		WebDriver driver =  Browser_class.start_browser_func("chrome","https://www.csusm.edu/");
+		App_elements access = new App_elements(driver);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		access.generalFunc();
+		js.executeScript("window.scrollBy(0,1000)");
+		Thread.sleep(2000);
+		Utility.captureScreenshots(driver, "Cougar Course");
+		
+		access.type_usernamefunc("sharm014");
+		Utility.captureScreenshots(driver, "username");
+		access.type_passwordfunc("Autotest@01");
+		Utility.captureScreenshots(driver, "password");
+		access.click_signIn_func();	
+		access.univ_course_func();
+		
+		driver.quit();
+
+		
+	}
+
+	
+}
