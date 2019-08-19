08f2095b39b2968e320ee4748793448c5c92917e
nishthasharma01 <54005937+nishthasharma01@users.noreply.github.com>
2019-08-18 20:36:33 -0700

    Update App_elements.java

--- App_elements.java
+++ App_elements.java
@@ -1,5 +1,7 @@
 package Elements_methods;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -10,20 +12,37 @@ public class App_elements {
 
 WebDriver driver;
 
+//login elements
 By campus_apps = By.xpath("/html/body/div/section[2]/div/div[2]/div/div[3]/div[2]/a/span[1]");
 By cougar_courses_btn = By.xpath("//*[@id='app-dd']/ul/li[2]/a/span");
-By login_btn = By.xpath("//*[@id='above-header']/div/div[2]/form/button");
-By username= By.xpath("//*[@id='userNameInput']");
-By password = By.xpath("//*[@id='passwordInput']");
-By signIn_btn = By.xpath("//*[@id='submitButton']");
-By lib_thesis_course = By.xpath("//*[@id='course-info-container-17399']/div/div[2]/h4/a");
-By home_tab = By.xpath("//*[@id='navwrap']/div/div/nav/div/div/ul[1]/li[1]/a");
-By dashBoard_tab = By.xpath("//*[@id='navwrap']/div/div/nav/div/div/ul[1]/li[2]/a");
-By previousCourse_tab = By.xpath("//*[@id='navwrap']/div/div/nav/div/div/ul[2]/li[3]/a");
-By community_tab = By.xpath("//*[@id='navwrap']/div/div/nav/div/div/ul[2]/li[4]/a");
-By logout_dropdown = By.xpath("//*[@id='above-header']/div/div[2]/div/a");
-By logout_btn = By.xpath("//*[@id='above-header']/div/div[2]/div/ul/li[7]/a");
+By login_btn2 = By.xpath("//*[@id='above-header']/div/div[2]/form/button");
+By login_btn1 = By.xpath("//*[@id='mr-nav']/div/a");
+By username2= By.xpath("//*[@id='userNameInput']");
+By username1= By.xpath("//*[@id='username']");
+By password2 = By.xpath("//*[@id='passwordInput']");
+By password1 = By.xpath("//*[@id='password']");
+By signIn_btn2 = By.xpath("//*[@id='submitButton']");
+By signIn_btn1 = By.xpath("//*[@id='snap-pm-content']/form/input[2]");
+By signIn_btn3 = By.xpath("//*[@id='region-main']/div/div/div/center/form/input");
+
+// All tabs elements
+By profile_tab = By.xpath("//*[@id='snap-pm-header-quicklinks']/a[1]");
+By dashBoard_tab = By.xpath("//*[@id='snap-pm-header-quicklinks']/a[2]");
+By prefrence_tab = By.xpath("//*[@id='snap-pm-header-quicklinks']/a[3]");
+By grades_tab = By.xpath("//*[@id='snap-pm-header-quicklinks']/a[4]");
+
+By myCourse_btn = By.xpath("//*[@id='snap-pm-trigger']/span[1]");
+
+//By home_tab = By.xpath("//*[@id='navwrap']/div/div/nav/div/div/ul[1]/li[1]/a");
+//By dashBoard_tab = By.xpath("//*[@id='navwrap']/div/div/nav/div/div/ul[1]/li[2]/a");
+//By previousCourse_tab = By.xpath("//*[@id='navwrap']/div/div/nav/div/div/ul[2]/li[3]/a");
+//By community_tab = By.xpath("//*[@id='navwrap']/div/div/nav/div/div/ul[2]/li[4]/a");
+//By logout_dropdown = By.xpath("//*[@id='above-header']/div/div[2]/div/a");
+
+
+By logout_btn = By.xpath("//*[@id='snap-pm-logout']");
 By invalid_credential = By.xpath("//*[@id='errorText']");
+By course_lib_thesis = By.xpath("//*[@id='snap-pm-courses-current-cards']/div[1]/div/h3/a");
 
 //constructor
 public App_elements(WebDriver driver)
@@ -37,32 +56,36 @@ public void generalFunc()
 	driver.findElement(campus_apps).click();
 	js.executeScript("window.scrollBy(0,document.body.scrollHeight);");
 	driver.findElement(cougar_courses_btn).click();
-	driver.findElement(login_btn).click();
+	driver.findElement(login_btn1).click();
+	driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	driver.findElement(username1).sendKeys("sharm014");
+	driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	driver.findElement(password1).sendKeys("Autotest@01");
+	driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	driver.findElement(signIn_btn1).click();
+	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	driver.findElement(signIn_btn3).click();
 }
 
 public void type_usernamefunc(String uid)
 {
-	driver.findElement(username).sendKeys(uid);
+	driver.findElement(username2).sendKeys(uid);
 }
 
 public void type_passwordfunc(String pass_word)
 {
-	driver.findElement(password).sendKeys(pass_word);
+	driver.findElement(password2).sendKeys(pass_word);
 }
 
 public void click_signIn_func()
 {
-	driver.findElement(signIn_btn).click();
+	driver.findElement(signIn_btn2).click();
 }
 
-public void click_course_func()
-{
-	driver.findElement(lib_thesis_course).click();
-}
 
-public void home_tab_func()
+public void profile_tab_func()
 {
-	driver.findElement(home_tab).click();
+	driver.findElement(profile_tab).click();
 }
 
 public void dashboard_tab_func()
@@ -70,20 +93,30 @@ public void dashboard_tab_func()
 	driver.findElement(dashBoard_tab).click();
 }
 
-public void previousCourse_tab_func()
+public void prefrence_tab_func()
 {
-	driver.findElement(previousCourse_tab).click();
+	driver.findElement(prefrence_tab).click();
 }
 
-public void community_tab_func()
+public void grades_tab_func()
 {
-	driver.findElement(community_tab).click();
+	driver.findElement(grades_tab).click();
 }
 
 public void logout_func()
 {
-	driver.findElement(logout_dropdown).click();
+	//driver.findElement(logout_dropdown).click();
 	driver.findElement(logout_btn).click();
+}
+
+public void lib_course_func()
+{
+	driver.findElement(course_lib_thesis).click();
+}
+
+public void my_courseBtn_func()
+{
+	driver.findElement(myCourse_btn).click();
 }
 
 public void invalid_creds_func()
@@ -94,4 +127,5 @@ public void invalid_creds_func()
 }
 
 }
+
 
