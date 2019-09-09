cc80e6e4cce4a8024cfe08e610356d949a791f3e
nishthasharma01 <54005937+nishthasharma01@users.noreply.github.com>
2019-09-08 19:22:20 -0700

    Update App_elements.java

--- App_elements.java
+++ App_elements.java
@@ -1,12 +1,19 @@
 package Elements_methods;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+//import org.openqa.selenium.security.UserAndPassword;
 
 public class App_elements {
 
@@ -31,7 +38,12 @@ By dashBoard_tab = By.xpath("//*[@id='snap-pm-header-quicklinks']/a[2]");
 By prefrence_tab = By.xpath("//*[@id='snap-pm-header-quicklinks']/a[3]");
 By grades_tab = By.xpath("//*[@id='snap-pm-header-quicklinks']/a[4]");
 
+//Courses elements
 By myCourse_btn = By.xpath("//*[@id='snap-pm-trigger']/span[1]");
+
+//CSUSM Webpage elements
+By acadamics_tab = By.xpath("//*[@id='tnavmenu']/div[1]/div/div/div[2]/span");
+By course_catalog_tab = By.xpath("//*[@id='ga-hp-academics-course-lg']");
 
 //By home_tab = By.xpath("//*[@id='navwrap']/div/div/nav/div/div/ul[1]/li[1]/a");
 //By dashBoard_tab = By.xpath("//*[@id='navwrap']/div/div/nav/div/div/ul[1]/li[2]/a");
@@ -39,8 +51,7 @@ By myCourse_btn = By.xpath("//*[@id='snap-pm-trigger']/span[1]");
 //By community_tab = By.xpath("//*[@id='navwrap']/div/div/nav/div/div/ul[2]/li[4]/a");
 //By logout_dropdown = By.xpath("//*[@id='above-header']/div/div[2]/div/a");
 
-
-By logout_btn = By.xpath("//*[@id='snap-pm-logout']");
+By logout_btn = By.xpath("//*[@id='snap-pm-logout']"); 
 By invalid_credential = By.xpath("//*[@id='errorText']");
 By course_lib_thesis = By.xpath("//*[@id='snap-pm-courses-current-cards']/div[1]/div/h3/a");
 
@@ -50,67 +61,87 @@ public App_elements(WebDriver driver)
 	this.driver=driver;	
 }
 
-public void generalFunc()
+//Login Function
+public void generalFunc() throws IOException
 {
 	JavascriptExecutor js = (JavascriptExecutor) driver;
 	driver.findElement(campus_apps).click();
 	js.executeScript("window.scrollBy(0,document.body.scrollHeight);");
 	driver.findElement(cougar_courses_btn).click();
 	driver.findElement(login_btn1).click();
-	driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-	driver.findElement(username1).sendKeys("sharm014");
-	driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-	driver.findElement(password1).sendKeys("Autotest@01");
-	driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-	driver.findElement(signIn_btn1).click();
-	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-	driver.findElement(signIn_btn3).click();
+//	driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+//	driver.findElement(username1).sendKeys("sharm014");
+//	driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+//	driver.findElement(password1).sendKeys("Autotest@01");
+//	driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+//	driver.findElement(signIn_btn1).click();
+//	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//	driver.findElement(signIn_btn3).click();
+	
+//	driver.get("https://sharm014:Autoacces001@auth.csusm.edu");
+	
+//	driver.switchTo().alert().sendKeys("sharm014" + Keys.TAB + "Autotest@01");
+//	driver.switchTo().alert().accept();
+//	Runtime.getRuntime().exec("E:\\nishtha\\CSUSM third sem\\Major Project\\AutoIt_Script\\HandleAuthenticationWindow.exe");
+	
 }
 
+//Type username function
 public void type_usernamefunc(String uid)
 {
 	driver.findElement(username2).sendKeys(uid);
 }
 
+//Type password function
 public void type_passwordfunc(String pass_word)
 {
 	driver.findElement(password2).sendKeys(pass_word);
 }
 
+//Click the signin button function
 public void click_signIn_func()
 {
 	driver.findElement(signIn_btn2).click();
 }
 
 
+//Profile tab
 public void profile_tab_func()
 {
 	driver.findElement(profile_tab).click();
 }
 
+//Dashboard function
 public void dashboard_tab_func()
-{
+{driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	driver.findElement(dashBoard_tab).click();
 }
 
+//Preference tab function
 public void prefrence_tab_func()
 {
 	driver.findElement(prefrence_tab).click();
 }
 
+//Grades tab function
 public void grades_tab_func()
 {
 	driver.findElement(grades_tab).click();
 }
 
+//Logout button function
 public void logout_func()
 {
-	//driver.findElement(logout_dropdown).click();
+	driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 	driver.findElement(logout_btn).click();
 }
 
+//Library course function
 public void lib_course_func()
 {
+//	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//	Actions act=new Actions(driver);
+//	act.moveToElement((WebElement) course_lib_thesis).click().perform();
 	driver.findElement(course_lib_thesis).click();
 }
 
@@ -119,6 +150,17 @@ public void my_courseBtn_func()
 	driver.findElement(myCourse_btn).click();
 }
 
+public void academics_tab_func()
+{
+	driver.findElement(acadamics_tab).click();
+}
+
+public void course_catalog_func()
+{
+	driver.findElement(course_catalog_tab).click();
+}
+
+//Invalid credentials function
 public void invalid_creds_func()
 {
 	WebElement errorMessage= driver.findElement(By.xpath("//*[@id='errorText']"));
@@ -127,5 +169,4 @@ public void invalid_creds_func()
 }
 
 }
-
 
